71df6a534d943f99144e4651cca4849cda6a8474
Carlos Flores <cfcortes@ucol.mx>
2021-08-24 09:13:51 -0500

    Create base project structure

---
+++ lib/github/api.rb
@@ -0,0 +1,8 @@
+require "github/api/version"
+
+module Github
+  module Api
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/github/api_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Github::Api do
+  it "has a version number" do
+    expect(Github::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
